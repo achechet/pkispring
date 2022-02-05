df4c234419fd0d35c4fe360a23caf005325a7110
achechet <achechet@gmail.com>
2022-02-05 20:20:23 +0300

    Added Main page

---
+++ src/main/java/ru/chechetkin/pkispring/PkispringApplication.java
@@ -0,0 +1,13 @@
+package ru.chechetkin.pkispring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PkispringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PkispringApplication.class, args);
+	}
+
+}
